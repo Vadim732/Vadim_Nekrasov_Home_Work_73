b1b95dbe31e6f6bfd21df1976f4a43919adc8400
Kirill <kirillsaydanov@gmail.com>
2024-11-28 15:45:43 +0600

    Поправил вывод на страница для регистрации и логина

--- MyChat/MyChat/ViewModels/LoginViewModel.cs
+++ MyChat/MyChat/ViewModels/LoginViewModel.cs
@@ -4,11 +4,11 @@ namespace MyChat.ViewModels;
 
 public class LoginViewModel
 {
-    [Required(ErrorMessage = "Неверный логин или пароль!")]
+    [Required(ErrorMessage = "Неверный логин или почта!")]
     public string Identifier { get; set; }
     [Required(ErrorMessage = "Требуется пароль!")]
     [DataType(DataType.Password)]
-    [MinLength(6)]
+    [MinLength(6, ErrorMessage = "Минимальная длина пароля 6 символов")]
     public string Password { get; set; }
     public bool RememberMe { get; set; }
     public string? ReturnUrl { get; set; }
